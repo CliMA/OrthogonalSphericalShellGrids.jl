d7316e7b3bb9a312e4c8b4a02a8e467255b5ed78
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-11-07 09:30:26 -0500

    glmakie non working?

--- examples/warped_latitude_longitude_grid.jl
+++ examples/warped_latitude_longitude_grid.jl
@@ -1,10 +1,10 @@
-using OrthogonalSphericalShellGrids
 using Oceananigans
-using Oceananigans.Grids: generate_coordinate, R_Earth
+using Oceananigans.Grids: R_Earth, generate_coordinate
 using Oceananigans.Utils: get_cartesian_nodes_and_vertices
+using OrthogonalSphericalShellGrids
 using GLMakie
 
-grid = WarpedLatitudeLongitudeGrid(size = (360, 200, 1))
+grid = WarpedLatitudeLongitudeGrid(size = (360, 180, 1))
 cartesian_nodes, cartesian_vertices = get_cartesian_nodes_and_vertices(grid, Center(), Center(), Center())
 
 x, y, z = cartesian_nodes
@@ -12,5 +12,5 @@ x, y, z = cartesian_nodes
 fig = Figure()
 ax  = LScene(fig[1, 1])
 
-surface!(ax, x.*0.999, y.*0.999, z.*0.999, color = :blue)
-wireframe!(ax, x, y, z)
+surface!(ax, x.*0.9999, y.*0.9999, z.*0.9999, color = :blue)
+wireframe!(ax, x[1:2:end, 1:2:end], y[1:2:end, 1:2:end], z[1:2:end, 1:2:end])
