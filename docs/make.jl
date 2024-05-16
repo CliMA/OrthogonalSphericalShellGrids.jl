093fc8e7ee929162f066977335ee48f81920a75a
simone silvestri <silvestri.simone0@gmail.com>
2024-05-16 00:29:57 -0400

    another bugfix

--- docs/make.jl
+++ docs/make.jl
@@ -16,11 +16,8 @@ const OUTPUT_DIR   = joinpath(@__DIR__, "src/literated")
 example = "generate_grid.jl"
 example_filepath = joinpath(EXAMPLES_DIR, example)
 withenv("JULIA_DEBUG" => "Literate") do
-    start_time = time_ns()
     Literate.markdown(example_filepath, OUTPUT_DIR;
                       flavor = Literate.DocumenterFlavor(), execute = true)
-    elapsed = 1e-9 * (time_ns() - start_time)
-    @info @sprintf("%s example took %s to build.", example, prettytime(elapsed))
 end
 
 #####
@@ -29,7 +26,7 @@ end
 
 pages = [
     "Home" => "index.md",
-    "API" => "grids.md"
+    "API" => "grids.md",
     "Generate Grid" => "literated/generate_grid.md"
 ]
 
@@ -46,15 +43,10 @@ format = Documenter.HTML(collapselevel = 1,
 makedocs(sitename = "OrthogonalSphericalShellGrids.jl",
          authors = "Simone Silvestri",
          pages = pages,
+         format = format,
          doctest = true, # set to false to speed things up
          clean = true,
          checkdocs = :exports) # set to :none to speed things up
-
-makedocs( ; 
-    sitename = "OrthogonalSphericalShellGrids",
-    format = Documenter.HTML(),
-    pages
-)
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
