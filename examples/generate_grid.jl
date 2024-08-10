37fa255a50e90607e09202fa0903eadd522cf3be
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-08-10 11:57:20 +1000

    plot both north and south pole regions

--- examples/generate_grid.jl
+++ examples/generate_grid.jl
@@ -1,14 +1,15 @@
 # # Generate a Tripolar Grid
 #
-# This Examples shows how to generate and visualize a Tripolar grid using the `OrthogonalSphericalShellGrids`
-# and CairoMakie packages. The script is structured into several key sections, each performing a specific task in the process
-# of creating and displaying the grid.
+# This example demonstrates how to generate a tripolar grid using the OrthogonalSphericalShellGrids
+# package and how to visualize the grid using the CairoMakie package. The example is structured
+# into several key sections, each performing a specific task in the process of creating and
+# visualizing the grid.
 #
-# The script begins by importing the required Julia packages. 
-# OrthogonalSphericalShellGrids is used for generating the Tripolar grid, a type of grid used in geophysical and oceanographic modeling 
-# due to its ability to handle polar singularities effectively. 
-# CairoMakie is used for visualization purposes, allowing the creation of figures and plots to display the grid.
-#
+# We begin by importing the required Julia packages.
+# OrthogonalSphericalShellGrids is used for generating the tripolar grid, a type of grid used in
+# geophysical and oceanographic modeling due to its ability to handle polar singularities effectively.
+# CairoMakie is used for visualization purposes, allowing the creation of figures and plots to visualize
+# the grid.
 
 using OrthogonalSphericalShellGrids
 using OrthogonalSphericalShellGrids: Face, Center
@@ -17,19 +18,21 @@ using CairoMakie
 
 # ## Grid generation
 #
-# The grid is generated with a call to OrthogonalSphericalShellGrids.TripolarGrid, 
-# specifying the grid size and the latitude of the "north" singularities. In this case, the grid has a resolution of 2 degrees 
-# (180x90 grid points) and north singularities at 35 degrees latitude. This setup creates a grid that covers the globe with a 
-# specific focus on handling the complexities near the poles.
+# The grid is generated via `OrthogonalSphericalShellGrids.TripolarGrid` method by
+# specifying the grid size and the latitude of the "north" singularities. Here, the grid
+# has a lateral resolution of 6 degrees (90 x 45 grid points in the horizontal) and the
+# north hemisphere singularities at 60 degrees latitude. This setup creates a grid that
+# covers the globe with a specific focus on handling the complexities near the North pole.
 
-grid = OrthogonalSphericalShellGrids.TripolarGrid(size = (180, 90, 1), north_poles_latitude = 35)
+grid = OrthogonalSphericalShellGrids.TripolarGrid(size = (60, 30, 1), north_poles_latitude = 60)
 
-# ## Retriveing the nodes
+# ## Retrieving the nodes
 #
-# The lines retrieve Cartesian coordinates for two types of grid nodes: `Face`-`Face` nodes 
-# and `Center`-`Center`` nodes. This is done using the `get_cartesian_nodes_and_vertices` function, 
-# which converts the grid's spherical coordinates into Cartesian coordinates for easier manipulation and visualization. 
-# The coordinates are stored in variables xF, yF, zF for Face-Face nodes, and xC, yC, zC for Center-Center nodes.
+# The lines retrieve Cartesian coordinates for two types of grid nodes: `Face`--`Face` nodes 
+# and `Center`--`Center`` nodes. This is done using the `get_cartesian_nodes_and_vertices` method,
+# which converts the grid's spherical coordinates into Cartesian coordinates for easier manipulation
+# and visualization. The coordinates are stored in variables `xF`, `yF`, `zF` for Face-Face nodes,
+# and `xC`, `yC`, `zC` for Center-Center nodes.
 
 cartesian_nodes, _ = get_cartesian_nodes_and_vertices(grid, Face(), Face(), Center())
 xF, yF, zF = cartesian_nodes
@@ -40,23 +43,36 @@ xC, yC, zC = cartesian_nodes
 # ## Visualization
 # 
 # Finally, we can visualize the grid using two types of visual elements: surfaces and wireframes.
-# Surfaces are plotted slightly smaller (0.9999 scale) than the wireframes to create a layered effect,
-# enhancing the visual distinction between the grid's structure and its outline. 
-# The Face-Face nodes are visualized with blue surfaces and black wireframes, 
-# while the Center-Center nodes are visualized with blue surfaces and wireframes. 
+# Surfaces are plotted slightly smaller (at a scale of 0.99 instead of 1) than the wireframes
+# to create a layered effect, enhancing the visual distinction between the grid's structure and
+# its outline. The Face-Face nodes are visualized with blue surfaces and black wireframes,
+# while the Center-Center nodes are visualized with blue surfaces and wireframes.
 
-fig = Figure()
-ax  = LScene(fig[1, 1]; show_axis = false)
+fig = Figure(size=(1200, 600))
+axN = Axis3(fig[1, 1]; aspect=(1, 1, 1), elevation = 0.9, azimuth = 7, height=1500)
+axS = Axis3(fig[1, 2]; aspect=(1, 1, 1), elevation = -0.9, azimuth = 7, height=1500)
 
-surface!(ax, xF.*0.9999, yF.*0.9999, zF.*0.9999, color = :blue)
-wireframe!(ax, xF, yF, zF, color = :black)
+for ax in (axN, axS)
+    scale_factor = 0.99
+    surface!(ax, xF .* scale_factor, yF .* scale_factor, zF .* scale_factor, color = :blue)
+    wireframe!(ax, xF, yF, zF, color = :black)
 
-surface!(ax, xC.*0.9999, yC.*0.9999, zC.*0.9999, color = :blue)
-wireframe!(ax, xC, yC, zC, color = :blue)
+    surface!(ax, xC .* scale_factor, yC .* scale_factor, zC .* scale_factor, color = :blue)
+    wireframe!(ax, xC, yC, zC, color = :blue)
+
+    hidedecorations!(ax)
+    hidespines!(ax)
+end
+rowsize!(fig.layout, 1, Relative(2.5))
 
 save("tripolar_grid_nodes.png", fig)
 nothing #hide
 
 # ![](tripolar_grid_nodes.png)
 
-
+# On the left we see how the North hemisphere has two poles that are shifted at latitude 60; on
+# the right we see that the Southern hemisphere has its pole at the usual South Pole.
+#
+# This tripolar configuration are very typical for ocean modeling so that the pole singularity in
+# the Arctic ocean is moved away into the land mases of Russia and Canada; the South pole
+# singularity is already within the land mass of Antarctica so it's all good down there!
