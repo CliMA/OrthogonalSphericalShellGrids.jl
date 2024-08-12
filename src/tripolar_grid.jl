5af4668dd210d08e6ce9f6027f00312fd664a82c
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-08-12 12:49:36 +1000

    fix docstring + code alignment

--- src/tripolar_grid.jl
+++ src/tripolar_grid.jl
@@ -24,8 +24,10 @@ const TripolarGrid{FT, TX, TY, TZ, A, R, FR, Arch} = OrthogonalSphericalShellGri
 
 Construct a tripolar grid on a spherical shell.
 
-NOTE: due to the requirements of the folding at the north edge of the domain, 
-`size[1]` should be an even number.
+!!! warning "An optional title"
+    `size` is a 3-tuple of the grid size in longitude, latitude, and vertical directions.
+    Due to requirements of the folding at the north edge of the domain, the longitude size
+    of the grid (i.e., the first component of `size`) _must_ be an even number!
 
 Positional Arguments
 ====================
@@ -36,11 +38,11 @@ Positional Arguments
 Keyword Arguments
 =================
 
-- `size`: The number of cells in the (longitude, latitude, z) dimensions.
+- `size`: The number of cells in the (longitude, latitude, vertical) dimensions.
 - `southermost_latitude`: The southernmost `Center` latitude of the grid. Default is -80.
-- `halo`: The halo size in the (longitude, latitude, z) dimensions. Default is (4, 4, 4).
+- `halo`: The halo size in the (longitude, latitude, vertical) dimensions. Default is (4, 4, 4).
 - `radius`: The radius of the spherical shell. Default is `R_Earth`.
-- `z`: The z-coordinate range of the grid. Default is (0, 1).
+- `z`: The vertical z-coordinate range of the grid. Default is (0, 1).
 - `first_pole_longitude`: The longitude of the first ``north'' singularity. 
                           The second singularity will be located at `first_pole_longitude + 180ᵒ`.
 - `north_poles_latitude`: The latitude of the ``north'' singularities.
