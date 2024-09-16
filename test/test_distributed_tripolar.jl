60d3cde400a4816e9288e7bd3203c3b2cc065906
Simone Silvestri <silvestri.simone0@gmail.com>
2024-09-16 18:24:19 -0400

    fix distributed tests

--- test/test_distributed_tripolar.jl
+++ test/test_distributed_tripolar.jl
@@ -7,7 +7,7 @@ run_distributed_grid = """
     using MPI
     MPI.Init()
 
-    include("test/distributed_tests_utils.jl")
+    include("distributed_tests_utils.jl")
     arch = Distributed(CPU(), partition = Partition(2, 2))
     
     distributed_grid = TripolarGrid(arch; size = (100, 100, 1), z = (-1000, 0))
@@ -15,14 +15,15 @@ run_distributed_grid = """
 
     simulation = run_tripolar_simulation(distributed_grid)
 
-    if arch.local_rank == 0
-        η = reconstruct_global_field(simulation.model.free_surface.η)
-        u = reconstruct_global_field(simulation.model.velocities.u)
-        v = reconstruct_global_field(simulation.model.velocities.v)
+    η = reconstruct_global_field(simulation.model.free_surface.η)
+    u = reconstruct_global_field(simulation.model.velocities.u)
+    v = reconstruct_global_field(simulation.model.velocities.v)
 
-        fill_halo_regions!(η)
-        fill_halo_regions!(u)
-        fill_halo_regions!(v)
+    fill_halo_regions!(η)
+    fill_halo_regions!(u)
+    fill_halo_regions!(v)
+
+    if arch.local_rank == 0
         jldsave("distributed_tripolar.jld2"; η = η.data, u = u.data, v = v.data) 
     end
 
