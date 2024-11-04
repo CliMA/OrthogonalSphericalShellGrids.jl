10d3274b3f616579d74416fc1e351af57c12b0e9
Simone Silvestri <silvestri.simone0@gmail.com>
2024-11-04 10:42:16 +0100

    add some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -34,3 +34,25 @@ end
 include("test_tripolar_grid.jl")
 include("test_zipper_boundary_conditions.jl")
 
+function tripolar_model_timesteps(grid, free_surface)
+    
+    
+    time_step!(model, 1)
+
+    return true
+end
+
+@testset "Model tests..." begin
+    grid = TripolarGrid(size = (10, 10, 1))
+
+    # Wrong free surface
+    @test_throws ArgumentError HydrostaticFreeSurfaceModel(; grid)
+
+    free_surface = SplitExplicitFreeSurface(grid; substeps = 10)
+    model = HydrostaticFreeSurfaceModel(; grid, free_surface)
+
+    @test begin
+        time_step!(model, 1.0)
+        true
+    end
+end
