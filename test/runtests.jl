6f13a23757c078e4e3807f81befcdc0ebf14871a
Simone Silvestri <silvestri.simone0@gmail.com>
2024-12-20 17:07:38 +0100

    (0.2.1) Correct `FT` argument that was not respected (#62)
    * make sure typing is correct
    
    * add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,34 +6,38 @@ using Oceananigans.Utils: KernelParameters
 contiguousrange(::KernelParameters{spec, offset}) where {spec, offset} = contiguousrange(spec, offset)
 
 @testset "Unit tests..." begin
-    grid = TripolarGrid(size = (4, 5, 1), z = (0, 1), 
-                        first_pole_longitude = 75, 
-                        north_poles_latitude = 35,
-                        southernmost_latitude = -80)
+    for FT in (Float32, Float64)
+        grid = TripolarGrid(CPU(), FT; 
+                            size = (4, 5, 1), z = (0, 1), 
+                            first_pole_longitude = 75, 
+                            north_poles_latitude = 35,
+                            southernmost_latitude = -80)
 
-    @test grid isa TripolarGrid
+        @test eltype(grid) == FT
+        @test grid isa TripolarGrid
 
-    @test grid.Nx == 4
-    @test grid.Ny == 5
-    @test grid.Nz == 1
+        @test grid.Nx == 4
+        @test grid.Ny == 5
+        @test grid.Nz == 1
 
-    @test grid.conformal_mapping.first_pole_longitude == 75
-    @test grid.conformal_mapping.north_poles_latitude == 35
-    @test grid.conformal_mapping.southernmost_latitude == -80
+        @test grid.conformal_mapping.first_pole_longitude == 75
+        @test grid.conformal_mapping.north_poles_latitude == 35
+        @test grid.conformal_mapping.southernmost_latitude == -80
 
-    λᶜᶜᵃ = λnodes(grid, Center(), Center())
-    φᶜᶜᵃ = φnodes(grid, Center(), Center())
+        λᶜᶜᵃ = λnodes(grid, Center(), Center())
+        φᶜᶜᵃ = φnodes(grid, Center(), Center())
 
-    min_Δφ = minimum(φᶜᶜᵃ[:, 2] .- φᶜᶜᵃ[:, 1])
+        min_Δφ = minimum(φᶜᶜᵃ[:, 2] .- φᶜᶜᵃ[:, 1])
 
-    @test minimum(λᶜᶜᵃ) ≥ 0
-    @test maximum(λᶜᶜᵃ) ≤ 360
-    @test maximum(φᶜᶜᵃ) ≤ 90
+        @test minimum(λᶜᶜᵃ) ≥ 0
+        @test maximum(λᶜᶜᵃ) ≤ 360
+        @test maximum(φᶜᶜᵃ) ≤ 90
 
-    # The minimum latitude is not exactly the southermost latitude because the grid 
-    # undulates slightly to maintain the same analytical description in the whole sphere
-    # (i.e. constant latitude lines do not exist anywhere in this grid)
-    @test minimum(φᶜᶜᵃ .+ min_Δφ / 10) ≥ grid.conformal_mapping.southernmost_latitude 
+        # The minimum latitude is not exactly the southermost latitude because the grid 
+        # undulates slightly to maintain the same analytical description in the whole sphere
+        # (i.e. constant latitude lines do not exist anywhere in this grid)
+        @test minimum(φᶜᶜᵃ .+ min_Δφ / 10) ≥ grid.conformal_mapping.southernmost_latitude 
+    end
 end
 
 include("test_tripolar_grid.jl")
