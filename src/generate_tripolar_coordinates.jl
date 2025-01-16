633112610eb67412e8b4f8555ba670029a12474e
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-01-16 14:43:33 +1100

    Add inbounds, loop along columns, fix docstring (#63)
    * add inbounds, loop along columns
    
    * fix hyperbolae/ellipses eqs to match Murray 1996
    
    * minor cleanup
    
    * minor tweak
    
    * some cos/sin -> cosd/sind
    
    * sind/cosd are custom
    
    * sind/cosd are custom
    
    * some space

--- src/generate_tripolar_coordinates.jl
+++ src/generate_tripolar_coordinates.jl
@@ -1,6 +1,6 @@
 """
-    _compute_tripolar_coordinates!(λFF, φFF, λFC, φFC, λCF, φCF, λCC, φCC, 
-                                   λᶠᵃᵃ, λᶜᵃᵃ, φᵃᶠᵃ, φᵃᶜᵃ, 
+    _compute_tripolar_coordinates!(λFF, φFF, λFC, φFC, λCF, φCF, λCC, φCC,
+                                   λᶠᵃᵃ, λᶜᵃᵃ, φᵃᶠᵃ, φᵃᶜᵃ,
                                    first_pole_longitude,
                                    focal_distance, Nλ)
 
@@ -15,39 +15,39 @@ The `focal_distance` argument is the distance from the center of the ellipses to
 The family of ellipses obeys:
 
 ```math
-\\frac{x²}{a² \\cosh²(ψ)} + \\frac{y²}{a² \\sinh²(ψ)} = 1
+\\frac{x²}{a² \\cosh² ψ} + \\frac{y²}{a² \\sinh² ψ} = 1
 ```
 
-While the family of perpendicular hyperbolae obey:
+while the family of perpendicular hyperbolae obey:
 
 ```math
-\\frac{x²}{a² \\cosh²(λ)} + \\frac{y²}{a² \\sinh²(λ)} = 1
+\\frac{x²}{a² \\mathrm{sind}² λ} - \\frac{y²}{a² \\mathrm{cosd}² λ} = 1
 ```
 
-Where ``a`` is the `focal_distance` to the center, ``λ`` is the longitudinal angle,
+Above, ``a`` is the `focal_distance` to the center, ``λ`` is the longitudinal angle,
 and ``ψ`` is the "isometric latitude", defined by Murray (1996) and satisfying:
 
 ```math
-    a \\sinh(ψ) = \\mathrm{tand}[(90 - φ) / 2]
+    a \\sinh ψ = \\mathrm{tand}[(90 - φ) / 2]
 ```
 
 The final ``(x, y)`` points that define the stereographic projection of the tripolar
 coordinates are given by:
 
 ```math
-    \\begin{align}
-    x & = a \\sinh ψ \\cos λ \\\\
-    y & = a \\sinh ψ \\sin λ
-    \\end{align}
+    \\begin{align*}
+    x & = a \\sinh ψ \\, \\mathrm{cosd} λ \\\\
+    y & = a \\sinh ψ \\, \\mathrm{sind} λ
+    \\end{align*}
 ```
 
-for which it is possible to retrieve the longitude and latitude by:
+from which we can recover the longitude and latitude as:
 
 ```math
-    \\begin{align}
-    λ &=    - \\frac{180}{π} \\mathrm{atan}(y / x)  \\\\
-    φ &= 90 - \\frac{360}{π} \\mathrm{atan} \\sqrt{x² + y²}
-    \\end{align}
+    \\begin{align*}
+    λ & =    - \\frac{180}{π} \\mathrm{atan}(y / x)  \\\\
+    φ & = 90 - \\frac{360}{π} \\mathrm{atan} \\sqrt{x² + y²}
+    \\end{align*}
 ```
 """
 @kernel function _compute_tripolar_coordinates!(λFF, φFF, λFC, φFC, λCF, φCF, λCC, φCC,
@@ -72,16 +72,16 @@ for which it is possible to retrieve the longitude and latitude by:
         # This makes sense, what is the longitude of the north pole? Could be anything!
         # so we choose a value that is continuous with the surrounding points.
         on_the_north_pole = (x == 0) & (y == 0)
-        north_pole_value  = ifelse(i == 1, -90, 90) 
+        north_pole_value  = ifelse(i == 1, -90, 90)
 
         λ2D[i, j] = ifelse(on_the_north_pole, north_pole_value, - 180 / π * atan(y / x))
-        φ2D[i, j] = 90 - 360 / π * atan(sqrt(y^2 + x^2)) # The latitude will be in the range [-90, 90]
+        φ2D[i, j] = 90 - 360 / π * atan(sqrt(y^2 + x^2)) # The latitude is in the range [-90, 90]
 
-        # Shift longitude to the range [-180, 180], the 
-        # the north singularities will be located at -90 and 90
-        λ2D[i, j] += ifelse(i ≤ Nλ÷2, -90, 90) 
+        # Shift longitude to the range [-180, 180],
+        # the north singularities are located at -90 and 90.
+        λ2D[i, j] += ifelse(i ≤ Nλ÷2, -90, 90)
 
-        # Make sure the singularities are at longitude we want them to be at.
+        # Make sure the singularities are at longitudes we want them to be at.
         # (`first_pole_longitude` and `first_pole_longitude` + 180)
         λ2D[i, j] += first_pole_longitude + 90
         λ2D[i, j]  = convert_to_0_360(λ2D[i, j])
